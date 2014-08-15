2579b459d8a58a9ff214a6f036c754be51f2e973
Vishnu <pingvishnu@gmail.com>
2014-08-14 21:38:14 -0700

    workaround unrestricted policy and check classpath

--- src/main/java/com/test/pgp/bc/BCPGPTest.java
+++ src/main/java/com/test/pgp/bc/BCPGPTest.java
@@ -9,6 +9,36 @@ public class BCPGPTest {
 
 	public static void main(String[] args) throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
+		
+		
+				Security.addProvider(new BouncyCastleProvider());
+		
+	
+	// Quick check to make sure the libraries are in classpath	
+	ClassLoader loader = org.bouncycastle.crypto.Digest.class.getClassLoader();
+	System.out.println(loader.getResource("org/bouncycastle/crypto/Digest.class"));
+		
+        String providerName = "BC";
+        
+        if (Security.getProvider(providerName) == null)
+        {
+            System.out.println(providerName + " provider not installed");
+        }
+        else
+        {
+            System.out.println(providerName + " is installed.");
+        }
+
+        // Evade Unrestricted policy instead or download and install unrestricted policy files
+        try {
+            Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
+            field.setAccessible(true);
+            field.set(null, java.lang.Boolean.FALSE);
+        } catch (Exception ex) {
+
+        }
+
+		
 		encryptFile();
 		decryptFile();
 		encryptAndSignFile();
@@ -31,7 +61,8 @@ public class BCPGPTest {
 		decryptor.setPassword("password");
 		decryptor.decryptFile("test.txt.enc", "test.txt.dec");
 	}
-		public static void encryptAndSignFile() throws Exception {
+	
+	public static void encryptAndSignFile() throws Exception {
 		BCPGPEncryptor encryptor = new BCPGPEncryptor();
 		encryptor.setArmored(false);
 		encryptor.setCheckIntegrity(true);
@@ -41,7 +72,8 @@ public class BCPGPTest {
 		encryptor.setSigningPrivateKeyPassword("password");
 		encryptor.encryptFile("./test.txt", "./test.txt.signed.enc");
 	}
-		public static void decryptSignedFile() throws Exception {
+	
+	public static void decryptSignedFile() throws Exception {
 		BCPGPDecryptor decryptor = new BCPGPDecryptor(); 
 		decryptor.setPrivateKeyFilePath("test.gpg.prv");
 		decryptor.setPassword("password");
@@ -51,7 +83,8 @@ public class BCPGPTest {
 		// this file is encrypted with weili's public key and signed using wahaha's private key
 		decryptor.decryptFile("test.txt.signed.enc", "test.txt.signed.dec");
 	}
-		public static void decryptSignedFile1() throws Exception {
+	
+	public static void decryptSignedFile1() throws Exception {
 		BCPGPDecryptor decryptor = new BCPGPDecryptor(); 
 		decryptor.setPrivateKeyFilePath("test.gpg.prv");
 		decryptor.setPassword("password");
@@ -61,11 +94,13 @@ public class BCPGPTest {
 		// this file is encrypted with weili's public key and signed using wahaha's private key
 		decryptor.decryptFile("test.txt.signed.asc", "test.txt.signed.dec1");
 	}
-		public static void decryptSignedFileWithoutSignatureVerification() throws Exception {
+	
+	public static void decryptSignedFileWithoutSignatureVerification() throws Exception {
 		BCPGPDecryptor decryptor = new BCPGPDecryptor(); 
 		decryptor.setPrivateKeyFilePath("test.gpg.prv");
 		decryptor.setPassword("password");
 		
 		// this file is encrypted with weili's public key and signed using wahaha's private key
 		decryptor.decryptFile("test.txt.signed.asc", "test.txt.signed.dec2");
-	}}
+	}
+}
